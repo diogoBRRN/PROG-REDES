724a90d58f1ab140e2220d1fa78aa5d9536dd2b8
Diogo Carvalho <diogamer3@gmail.com>
2025-04-29 17:41:15 -0300

    media

---
+++ 2025-04-29/Media.py
@@ -0,0 +1,10 @@
+print ('Calcule a média aritmética de 4 nota')
+
+nota1 = 6.5
+nota2 = 7.0
+nota3 = 4.9
+nota4 = 6.3
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+
+print(f'A média é: {media:.1f}')
