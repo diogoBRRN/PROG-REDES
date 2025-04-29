91eb7f7a8b14fe583ce4b63d146b7e7561f732b8
Diogo Carvalho <diogamer3@gmail.com>
2025-04-29 17:33:34 -0300

    formatado

--- 2025-04-29/Area_circulo.py
+++ 2025-04-29/Area_circulo.py
@@ -1,9 +1,9 @@
 print('Calcular área do circulo')
 
-raio = 5
+raio = 5.13
 
 area = 3.14 * raio**2
 
 print('Área do circulo:', area)
 
-print('Área do circulo de raio:', raio, 'é', area)
+print(f'Área do circulo de raio {raio} é {area:.4f}')
