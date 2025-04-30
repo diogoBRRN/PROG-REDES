92bc6c6d8e6247ce30179e6c8c741ec753cd6ef6
Diogo Carvalho <diogamer3@gmail.com>
2025-04-30 17:23:31 -0300

    Create media.py

---
+++ 2025-04-30/media.py
@@ -0,0 +1,7 @@
+
+Nota1 = int (input('Digite a nota da primeira Unidade: '))
+Nota2 = int (input('Digite a nota da segunda Unidade: '))
+
+media = ((Nota1*2)+(Nota2*3))/5
+
+print(f'A média é igual a {media:.0f}')
