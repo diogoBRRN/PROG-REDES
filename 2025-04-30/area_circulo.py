71770f042f758e477f914b1e42c44d22bfc358e4
Diogo Carvalho <diogamer3@gmail.com>
2025-04-30 17:15:54 -0300

    Update area_circulo.py

--- 2025-04-30/area_circulo.py
+++ 2025-04-30/area_circulo.py
@@ -1,7 +1,11 @@
 # Programa para calcular a área do círculo
 
-raio= 5.3
+raio= float (input('Digite o raio do círculo: '))
 
 area = 3.14*raio**2
 
 print(f'Área do círculo é {area}')
+
+comprimento = 2*3.14*raio
+
+print(f'O comprimento da circunferência é {comprimento}')
