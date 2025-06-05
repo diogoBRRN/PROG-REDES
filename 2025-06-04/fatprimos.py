b12c68924417803f674997763adfe918e78accad
Diogo Carvalho <diogamer3@gmail.com>
2025-06-05 15:48:48 -0300

    fatprimos
    fat primos

--- 2025-06-04/fatprimos.py
+++ 2025-06-04/fatprimos.py
@@ -1,10 +1,30 @@
-'''
-   Os primeiros dois números consecutivos que podem ser decompostos 
-   como apenas dois fatores primos são 14 = (2 * 7) e 15 = (3 * 5).
+n = int(input('Digite uma quantidade de fatores primos que o(s) número(s) em questão deve ter: '))
+ 
+consecutivos = 0 #Contador que será incrementado sempre que um número tiver exatamente n fatores primos diferentes, tipo 2, 3 ,5 7 .....
+comeco = 2 # contador que guardara o primeiro número da sequência quando a condição for satisfeita.
 
-   Faça um programa que recebe a quantidade n de fatores primos que 
-   um número pode ter. 
+for numero in range(2, 99999): #Verifica todos os números de 2 até essse número gigante, procuramos os primeiros n (número escolhido pelo usuário) números consecutivos que tenham também n fatores primos diferentes.
+   num_temp = numero 
+   cont = 0
+   i = 2
+
+   for i in range(2, numero + 1):
+      if (i * i) > num_temp: 
+         break
+      if num_temp % i == 0:
+         cont+=1
+         while num_temp % i == 0:
+            num_temp //= i
    
-   O programa deve listar os primeiros n números consecutivos que 
-   atendem ao critério.
-'''
+   if num_temp > 1:
+      cont += 1
+   if cont == n: 
+      consecutivos += 1
+      if consecutivos == n:
+         comeco = numero - n + 1
+         break
+      else:
+         consecutivos=0
+
+for x in range(n):
+   print(comeco + x)
